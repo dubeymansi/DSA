4d10ec451d19a8ffafb7117407e68c72355f31fc
PALAK SINGH <80193531+Palaksingh02@users.noreply.github.com>
2022-06-17 01:24:19 +0530

    updates

--- BinarySearch/278.first-bad-version.java
+++ BinarySearch/278.first-bad-version.java
@@ -8,7 +8,7 @@
 /* The isBadVersion API is defined in the parent class VersionControl.
       boolean isBadVersion(int version); */
 
-public class Solution extends VersionControl {
+class Solution extends VersionControl {
     public int firstBadVersion(int n) {
         int start=1;
         int end=n;
